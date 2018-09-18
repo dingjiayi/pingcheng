bd70122ec8e51cd4dc236a48f7b0ade147ca0200
dingjiayi <1548534563@qq.com>
2018-09-18 20:54:48 +0800

    init repository

---
+++ base/common/noncopyable.h
@@ -0,0 +1,18 @@
+#ifndef PINGCHENG_BASE_COMMON_NONCOPYABLE_H
+#define PINGCHENG_BASE_COMMON_NONCOPYABLE_H
+
+namespace pingcheng {
+    namespace noncopyable_ {
+    
+        class noncopyable {
+        protected:
+            noncopyable() = default;
+            ~noncopyable() = default;
+            noncopyable(const noncopyable&) = delete;
+            noncopyable& operator=(const noncopyable&) = delete;
+        }
+    }
+
+}
+
+#endif
