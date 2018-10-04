c677aaed2a41f503f670373be1c34b0c01b43331
dingjiayi <1548534563@qq.com>
2018-10-04 20:12:45 +0800

    finish Timestamp.h

--- base/common/Timestamp.h
+++ base/common/Timestamp.h
@@ -19,14 +19,89 @@ class Timestamp : public boost::equality_comparable<Timestamp>,
 {
 public:
 // Constructs an invalid Timestamp.
-Timestamp() :
+Timestamp() : microSecondsSinceEpoch_(0);
+{}
+
+// Constructs a Timestamp at specific time
+// @param microSecondsZSinceEpoch_
+explicit Timestamp(int64_t microSecondsSinceEpochArg) : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
+{}
+
+void swap(Timestamp& that)
+{
+    std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
+}
+
+// default copy/ assignment/dtor are Okay
+
+std::string toString() const;
+string toFromattedString(bool showMicroseconds = true) const;
+bool valid() const { return microSecondsSinceEpoch_ > 0};
+
+// for internal usage.
+int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
+time_t secondsSinceEpoch() const 
+{
+    return static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond); )
+}
+
+// Get time of now
+
+static Timestamp now();
+static Timestamp invalid()
+{
+    return Timestamp();
+}
+
+static Timestamp fromUnixTime(time_t t)
+{
+    return from UnixTime(t, 0);
+}
+
+static Timestamp fromUnixTime(time_t t, int microseconds)
+{
+    return Timestamp(static_cast<int64_t>(t) * kMicroSecondsPerSecond + microseconds);
+}
+
+static const int kMicroSecondsPerSecond = 1000 * 1000;
 
 private:
     int64_t microSecondsSinceEpoch_;
 }
 
+inline bool operator<(Timestamp lhs, Timestamp rhs)
+{
+    return lhs.microSecondsSinceEpoch() < rsh.microSecondsSinceEpoch();
+}
+
+inline bool operator(Timestamp lhs, Timestamp rhs)
+{
+    return lhs.microSecondsSinceEpoch() == rsh.microSecondsSinceEpoch();
+}
+
+// Gets time difference of two timestamps, result in seconds.
+
+// @param high, low
+// @return (high - low) in seconds
+// @c double has 52-bit precision, enough for one-microsecond
+// resolution for next 100 years.
+
+inline double timeDifference(Timestamp high, Timestamp low)
+{
+    int64_t diff = hith.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
+    return static_cast<double>(diff) / Timestamp::kMicroSecondsPerSecond;
 
 }
 
+// Add @c seconds to given timestamp.
+// @return timestamp + seconds as Timestamp
 
-https://www.cnblogs.com/Anker/p/3418792.html
+inline Timestamp addTime(Timestamp timestamp, double seconds)
+{
+    int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+    return Timestamp(timestamp.microSecondsSinceEpoch() + delta; )    
+}
+
+
+
+} // namespace pingcheng
