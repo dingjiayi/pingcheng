fe69ecaf3b6ba4b9480c492904b6b7bc8548c167
dingjiayi <1548534563@qq.com>
2018-10-09 21:04:41 +0800

    add SocketsOps.h

---
+++ net/SocketsOps.h
@@ -0,0 +1,23 @@
+
+// This is an internal header file, you should not include this.
+
+#ifndef PC_NET_SOCKETSOPS_H
+#define PC_NET_SOCKETSOPS_H
+
+#include <arpa/inet.h>
+
+namespace pingcheng 
+{
+namespace net
+{
+namespace sockets 
+{
+/// 
+/// Creates a non-blocking socket file descriptor,
+/// abort if any error.
+
+} // namespace sockets
+} // namespace net
+} // namespace pingcheng 
+
+#endif // PC_NET_SOCKETSOPS_H
