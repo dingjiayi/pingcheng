a74c8dcd6926756b8443433ec25073d1e2948cba
dingjiayi <1548534563@qq.com>
2018-10-15 22:15:34 +0800

    finish file: net\Acceptor.h

--- net/Acceptor.h
+++ net/Acceptor.h
@@ -20,8 +20,25 @@ class Acceptor : noncopyable
 {
 public:
     typedef std::function<void (int sockfd, const InetAddress& addr)> NewConnectionCallback;
-    
-} 
+    Acceptor(EventLoop* loop, const InetAddress& listenAddr, bool reuseport);
+    ~Acceptor();
+
+    void setNewConnectionCallback(const NewConnectionCallback& cb)
+    {
+        newConnectionCallback_ = cb;
+    }
+
+    void listenning_() const { return listenning_;}
+    void listen();
+private:
+    void handleRead();
+    EventLoop* loop_;
+    Socket acceptSocket_;
+    Channel acceptChannel_;
+    NewConnectionCallback newConnectionCallback_;
+    bool listenning_;
+    int idleFd_;
+};
 
 
 } // namespace pingcheng
