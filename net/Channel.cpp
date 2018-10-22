01195c9ef2617e256970fcc49f147221482837ef
dingjiayi <1548534563@qq.com>
2018-10-22 10:24:36 +0800

    finish Channel.cpp

--- net/Channel.cpp
+++ net/Channel.cpp
@@ -4,6 +4,7 @@
 #include "net/EventLoop.h"
 
 #include <poll.h>
+#include <sstream>
 
 using namespace pingcheng;
 using namespace pingcheng::net;
@@ -37,6 +38,110 @@ Channel::~Channel()
 
     if (loop_ -> isInLoopThread())
     {
-        assert()
+        assert(!loop_->hasChannel(this));
     }
+}
+
+void Channel::tie(const std::shared_ptr<void>& obj)
+{
+    tie_ = obj;
+    tied_ = true;
+}
+
+void Channel::update()
+{
+    addedToLoop_ = true;
+    loop_ ->updateChannel(this);
+}
+
+void Channel::remove()
+{
+    assert(isNonoEvent());
+    addedToLoop_ = false;
+    loop_ -> removeChannel(this);
+}
+
+void Channel::handleEvent(Timestamp receiveTime)
+{
+    std::shared_ptr<void> guard;
+    if (tied_)
+    {
+        guard = tie_.lock();
+        if (guard)
+        {
+            handleEventWithGuard(receiveTime);
+        }
+
+    }
+    else
+    {
+        handleEventWithGuard(receiveTime);
+    }
+}
+
+void Channel::handleEventWithGuard(Timestamp receiveTime)
+{
+    eventHandling_ = true;
+    LOG_TRACE << reventsToString();
+    if ((revents_ & POLLHUP) && !(revents_ & POLLIN))
+    {
+        if (logHup_)
+        {
+            LOG_WARN << "fd = " << fd_ << " Channel::handle_event() POLLHUP";
+        }
+        if (closeCallback_) closeCallback_();
+    }
+
+    if (revents_ & POLLNVAL)
+    {
+        LOG_WARN << "fd = " << fd_ << " Channel::handle_event() POLLNVAL";
+    }
+
+    if (revents_ & (POLLERR | POLLNVAL))
+    {
+        if (errorCallback_) errorCallback_();
+    }
+
+    if (revents_ & (POLLIN | POLLPRI | POLLRDHUP))
+    {
+        if (readCallback_) readCallback_(receiveTime);
+    }
+
+    if (revents_ &  POLLOUT)
+    {
+        if (writeCallback_) writeCallback_()
+    }
+
+    eventHandling_ = false;
+}
+
+std::string Channel::reventsToString() const
+{
+    return eventsToString(fd_, revents_);
+}
+
+std::string Channel::eventsToString() const
+{
+    return eventsToString(fd_, events);
+}
+
+std::string Channel::eventsToString(int fd, int ev)
+{
+    std::ostringstream oss;
+
+    oss << fd << ": ";
+    if (ev & POLLIN)
+        oss << "IN ";
+    if (ev & POLLPRI)
+        oss << "PRI ";
+    if (ev & POLLOUT)
+        oss << "OUT ";
+    if (ev & POLLHUP)
+        oss << "HUP ";
+    if (ev & POLLERR)
+        oss << "ERR ";
+    if (ev & POLLNVAL)
+        oss << "NVAL ";
+
+    return oss.str().c_str();
 }
