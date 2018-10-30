959fd21c5de6f2612ac3d95a6023ba0b1c6ee378
dingjiayi <1548534563@qq.com>
2018-10-30 21:49:59 +0800

    update base/Thread/Atomic.h

--- base/Thread/Atomic.h
+++ base/Thread/Atomic.h
@@ -19,15 +19,58 @@ public:
 
     T get()
     {
-        // in gcc >= 4.7: __atomic_load_n(&value_, __ATOMIC_SEQ_CST)
-        return __sync_val_compare_and_swap(&value_, 0, 0);
+        // in gcc >= 4.7: 
+        return __atomic_load_n(&value_, __ATOMIC_SEQ_CST)
+        //return __sync_val_compare_and_swap(&value_, 0, 0);
     }
 
-    
+    T getAndAdd(T x)
+    {
+        // in gcc >= 4.7 
+        return __atomic_fetch_add(&value_, x, __ATOMIC_SEQ_CST);
+        // return __sync_fetch_and_add(&value_, x);
+    }
+
+    T addAndGet(T x)
+    {
+        return getAndAdd(x) + x; 
+    }
+
+    T incrementAndGet()
+    {
+        return addAndGet(1);
+    }
+
+    T decrementAndGet()
+    {
+        return addAndGet(-1);
+    }
+
+    void add(T x)
+    {
+        getAndAdd(x);
+    }
+
+    void increment()
+    {
+        decrementAndGet();
+    }
+
+    T getAndSet(T newValue)
+    {
+        // in gcc>= 4.7 
+        return __atomic_exchange_n(&value, newValue, __ATOMIC_SEQ_CST);
+        // return __sync_lock_test_and_set(&value_, newValue);
+    }
+
+
 private:
     volatile T value_;
-} // class AtomicIntegerT
+}; // class AtomicIntegerT
 } // detail
+
+typedef detail::AtomicIntegerT<int32_t> AtomicInt32;
+typedef detail::AtomicIntegerT<int64_t> AtomicInt64;
 } // pingcheng 
 
 #endif // PC_BASE_THREAD_THREAD_H
